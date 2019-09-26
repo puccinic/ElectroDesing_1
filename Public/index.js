f27f10ddc8ae66b5e6469566bf220f9dc8abaa41
puccinic <puccinic@uninorte.edu.co>
2019-09-26 00:30:59 -0500

    configuración de settings para la get request

--- Public/index.js
+++ Public/index.js
@@ -10,48 +10,56 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 let markers = [];
 let latlngs = [];
 let marker = L.marker([51.5, -0.09]).addTo(mymap);
-let polyline = L.polyline(latlngs, {color: 'green'}).addTo(mymap);
+let polyline = L.polyline(latlngs, {
+    color: 'green'
+}).addTo(mymap);
 
-$.get('/Appdata').done(function (data){
+$.get('/Appdata').done(function(data) {
     const lat = `<b>latitud:</b> ${data.lat} `;
     const lon = `<b>longitud:</b> ${data.lon} `;
     const time = `<b>tiempo:</b> ${new Date(data.time).toString()} `;
     const texti = '<p>' + lat + lon + time + '</p>';
     $('#syrus').html(texti);
     const polyLength = polyline.getLatLngs().length;
-    const lastPos = polyline.getLatLngs()[polyLength-1];
-    if (polyline.isEmpty()||((data.lat != lastPos.lat) && (data.lon != lastPos.lng))){
+    const lastPos = polyline.getLatLngs()[polyLength - 1];
+    if (polyline.isEmpty() || ((data.lat != lastPos.lat) && (data.lon != lastPos.lng))) {
         polyline.addLatLng([data.lat, data.lon]);
         marker.setLatLng([data.lat, data.lon]);
-        markers.push(L.circleMarker([data.lat,data.lon],5).addTo(mymap).setRadius(1));
+        markers.push(L.circleMarker([data.lat, data.lon], 5).addTo(mymap).setRadius(1));
     }
 });
 
 
-window.setInterval(
-    $.get('/Appdata').succes(function (data){
-        const lat = `<b>latitud:</b> ${data.lat} `;
-        const lon = `<b>longitud:</b> ${data.lon} `;
-        const time = `<b>tiempo:</b> ${new Date(data.time).toString()} `;
-        const texti = '<p>' + lat + lon + time + '</p>';
-        $('#syrus').html(texti);
-        const polyLength = polyline.getLatLngs().length;
-        const lastPos = polyline.getLatLngs()[polyLength-1];
-        if (polyline.isEmpty()||((data.lat != lastPos.lat) && (data.lon != lastPos.lng))){
-            polyline.addLatLng([data.lat, data.lon]);
-            marker.setLatLng([data.lat, data.lon]);
-            markers.push(L.circleMarker([data.lat,data.lon],5).addTo(mymap).setRadius(1));
+window.setInterval(function() {
+    const settings = {
+        url: '/Appdata',
+        success: function(data) {
+            const lat = `<b>latitud:</b> ${data.lat} `;
+            const lon = `<b>longitud:</b> ${data.lon} `;
+            const time = `<b>tiempo:</b> ${new Date(data.time).toString()} `;
+            const texti = '<p>' + lat + lon + time + '</p>';
+            $('#syrus').html(texti);
+            const polyLength = polyline.getLatLngs().length;
+            const lastPos = polyline.getLatLngs()[polyLength - 1];
+            if (polyline.isEmpty() || ((data.lat != lastPos.lat) && (data.lon != lastPos.lng))) {
+                polyline.addLatLng([data.lat, data.lon]);
+                marker.setLatLng([data.lat, data.lon]);
+                markers.push(L.circleMarker([data.lat, data.lon], 5).addTo(mymap).setRadius(1));
+            }
         }
-    }), 5000);
+    };
+    $.get(settings);
+}, 5000);
 
 
-$('.myButton').click(function () {
+$('.myButton').click(function() {
     $('.info').text('');
-    const timeMargin = {initTime: new Date($('#init-date').val()).getTime(),
-                      finalTime : new Date($('#final-date').val()).getTime()
-                     };
+    const timeMargin = {
+        initTime: new Date($('#init-date').val()).getTime(),
+        finalTime: new Date($('#final-date').val()).getTime()
+    };
 
-    $.get('/search',timeMargin).done(function (data){
+    $.get('/search', timeMargin).done(function(data) {
         console.log(data);
     });
 
