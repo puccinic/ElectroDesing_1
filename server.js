72d6e86a5a497a6db063fa315beb913e8c48e223
Sebastian <sdmejia@uninorte.edu.co>
2019-09-28 01:53:23 -0500

    adicion menu

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const dgram = require('dgram');
 const express = require('express');
 const mysql = require('mysql');
 const decodSyrus = require('./decodSyrus.js');
-const IP_ADRESS = '10.20.6.103';
-const UDP_PORT = '5000';
+const IP_ADRESS = '192.168.1.67';
+const UDP_PORT = '50000';
 const TCP_PORT = 3000;
 
 const server = dgram.createSocket('udp4');
